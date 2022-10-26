a3b0a243450badbb6b569c4ffa6afa65e3192fee
Emanuelle-02 <85840803+Emanuelle-02@users.noreply.github.com>
2022-10-26 18:03:07 -0300

    Atividade model

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from main.models import *
+# Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Locacao)
+admin.site.register(Filme)
+admin.site.register(Categoria)
---
+++ main/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=150)
+    email = models.EmailField()
+    endereco = models.CharField(max_length=350)
+
+    def __str__(self):
+        return self.nome
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=250)
+    ano_lancamento = models.IntegerField()
+    valor_locacao = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Locacao(models.Model):
+    data_entrega = models.DateField()
+    data_locacao = models.DateField()
+    valor = models.IntegerField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fillme = models.ManyToManyField(Filme)
